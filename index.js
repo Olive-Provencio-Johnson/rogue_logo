beb39aa331d1cb684551883ccdb04f9e5add1730
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-03-22 23:55:24 -0700

    added prompts with inquierer.prompt. + fs.writeFile to eventually create the logo.

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
 // GIVEN a command-line application that accepts user input
-// -----WHEN I am prompted for text
-// -----THEN I can enter up to three characters
+// -----prompt for text = enter up to three characters
+// -----prompt for text color = enter a color keyword (OR a hexadecimal number)
+// -----prompt for logo shape = list of shapes to choose from: circle, triangle, and square
+// -----prompt for the shape's color =  enter a color keyword (OR a hexadecimal number)
 
-// WHEN I am prompted for the text color
-// ---- THEN I can enter a color keyword (OR a hexadecimal number)
+inquirer.prompt([
+{
+  type: 'input',
+  name: 'logo characters',
+  message: 'Please enter up to three characters for your logo',
+},
+{
+    type: 'input',
+    name: 'text color',
+    message: 'Please enter a color keyword or hexadecimal number to indicate your logo text color',
+  },
+  {
+    type: 'list',
+    name: 'logo shape',
+    message: 'Please choose your logo shape from the list provided here',
+    choices: ["Circle", "Triangle", "Square"], 
+  },
+  {
+    type: 'list',
+    name: 'shape color',
+    message: 'Please enter a color keyword or hexadecimal number to indicate your logo shape color',
+    choices: ["Circle", "Triangle", "Square"], 
+  },
+]);
 
-// WHEN I am prompted for a shape
-// ----THEN I am presented with a list of shapes to choose from: circle, triangle, and square
 
-// WHEN I am prompted for the shape's color
-// ----THEN I can enter a color keyword (OR a hexadecimal number)
+// SVG file is created named `logo.svg AND the output text "Generated logo.svg" is printed in the command line
+fs.writeFile('logo.svg', data, (err) => {
+    if (err) throw err;
+    console.log('Generated logo.svg');
+  });
 
-// WHEN I have entered input for all the prompts
-// ----THEN an SVG file is created named `logo.svg`
-//------ AND the output text "Generated logo.svg" is printed in the command line
 
-// WHEN I open the `logo.svg` file in a browser
+
+
+// **WHEN I open the `logo.svg` file in a browser
 // ----THEN I am shown a 300x200 pixel image that matches the criteria I entered
-
 
 // While not a requirement, it is recommended that you place any common functionality and properties shared by the Triangle, Circle, and Square classes in a parent Shape class and use inheritance to reuse the code in the child classes.
 
