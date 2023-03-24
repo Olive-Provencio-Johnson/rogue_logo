1ae81683f5fd6f8391feb4344ad8bb8b8bf67dad
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-03-24 01:22:13 -0700

    added cirlce and square constructor and template literals.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-
+const { Triagnle } = require("./lib/shapes");
 
 // GIVEN a command-line application that accepts user input
 // -----prompt for text = enter up to three characters
@@ -8,45 +8,64 @@ const fs = require("fs");
 // -----prompt for logo shape = list of shapes to choose from: circle, triangle, and square
 // -----prompt for the shape's color =  enter a color keyword (OR a hexadecimal number)
 
-inquirer.prompt([
-{
-  type: 'input',
-  name: 'logoCharacters',
-  message: 'Please enter up to three characters for your logo',
-},
-{
-    //how will this be processed in the terminal with basic input?
-    type: 'input',
-    name: 'textColor',
-    message: 'Please enter a color keyword or hexadecimal number to indicate your logo text color',
-  },
-  {
-    type: 'list',
-    name: 'logoShape',
-    message: 'Please choose your logo shape from the list provided here',
-    choices: ["Circle", "Triangle", "Square"], 
-  },
-  {
-    //how will this be processed in the terminal with basic input?
-    type: 'input',
-    name: 'shapeColor',
-    message: 'Please enter a color keyword or hexadecimal number to indicate your logo shape color',
-  },
-]).then((data) => {
-    console.log(data);
-})
+function run() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "logoCharacters",
+        message: "Please enter up to three characters for your logo",
+      },
+      {
+        type: "input",
+        name: "textColor",
+        message:
+          "Please enter a color keyword or hexadecimal number to indicate your logo text color",
+      },
+      {
+        type: "list",
+        name: "logoShape",
+        message: "Please choose your logo shape from the list provided here",
+        choices: ["Circle", "Triangle", "Square"],
+      },
+      {
+        type: "input",
+        name: "shapeColor",
+        message:
+          "Please enter a color keyword or hexadecimal number to indicate your logo shape color",
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+      let shape;
+      if (data.logoShape === "Triangle") {
+        shape = new Triangle(
+          data.textColor,
+          data.logoCharacters,
+          data.shapeColor,
+        );
+      } else if (data.logoShape === "Circle") {
+        shape = new Circle(
+            data.textColor,
+            data.logoCharacters, 
+            data.shapeColor,
+        );
+      } else if (data.logoShape === "Square") {
+        shape = new Square(
+            data.textColor,
+            data.logoCharacters,
+            data.shapeColor,
+        );
+      }
+      console.log(shape);
+      // SVG file is created named `logo.svg` AND the output text "Generated logo.svg" is printed in the command line
+      fs.writeFile("logo.svg", shape.print(), (err) => {
+        if (err) throw err;
+        console.log("Generated logo.svg");
+      });
+    });
+}
 
-
-// SVG file is created named `logo.svg AND the output text "Generated logo.svg" is printed in the command line
-// fs.writeFile('logo.svg', data, (err) => {
-//     if (err) throw err;
-//     console.log('Generated logo.svg');
-//   });
-
-
-
-
-
-// While not a requirement, it is recommended that you place any common functionality and properties shared by the Triangle, Circle, and Square classes in a parent Shape class and use inheritance to reuse the code in the child classes.
+run();
 
 // Each shape class should be tested for a render() method that returns a string for the corresponding SVG file with the given shape color.
